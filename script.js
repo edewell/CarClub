8046d653b8325adb2e2bedc0508c226549c8136d
edewell <edewell3@gmail.com>
2021-07-01 17:22:42 -0500

    carclub

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ alert("Hello");
 
 let company = prompt('Which company created the 350z?', '');
 
-(company == 'Nissan') ?
-   alert('Right!') : alert('Wrong.');
 
+   if (company == 'Nissan'){
+      alert("right");
+   }
+   else if (company !== 'Nissan'){
+      alert("wrong");
+   }
 
+let question = prompt('Are you a car lover?', "");
+   
 
+      if (question == 'yes'){
+         alert("Welcome");
+      }
+      else if (question !== 'yes'){
+         alert("its ok")
+      }
+
+let guessinggame = prompt("I am thinking of American Muscle");
+
+      
+      if (guessinggame == "Dodge"){
+         alert("right");
+      }
+      else if (guessinggame !== "Dodge"){
+         alert("wrong")
+      }
 
 
